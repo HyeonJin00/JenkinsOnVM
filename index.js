47657428b250521a0f15e00b6b897d59c5184cac
HyeonJin00 <86398907+HyeonJin00@users.noreply.github.com>
2021-06-25 17:44:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like bp!');
+    res.send('I like ohmygirl!');
 });
 
 app.get('/start', function(req, res){
